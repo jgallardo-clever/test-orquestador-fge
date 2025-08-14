10fbf5a741096c4cde049017d2c8c62316ab4066
Jorge Gallardo <jorgito899@gmail.com>
2025-08-14 09:14:57 -0400

    Se agrega el deploy de php laravel

---
+++ .github/actions/utils/iis-site/add-default-document/add-default-document.ps1
@@ -0,0 +1,63 @@
+##########################################################################
+# Configurar documento por defecto en IIS
+##########################################################################
+
+param (
+    [string]$siteName,
+    [string]$defaultDocument,
+
+    [string]$ipServer,
+    [string]$sshUser
+
+)
+
+##########################################################################
+# Validación de parámetros
+##########################################################################
+
+# Si no se especifica ipServer ni sshUser, se cancela la ejecución
+if (-not $ipServer -or -not $sshUser -or -not $siteName -or -not $defaultDocument) {
+    Write-Error "ipServer, sshUser, siteName, and defaultDocument must be specified."
+    return
+}
+
+##########################################################################
+# Script remoto para crear el Application Pool
+##########################################################################
+
+# Cargamos el script remoto que se ejecutará en el servidor IIS
+$remoteScript = @"
+# Importamos el módulo WebAdministration para gestionar IIS
+try {
+    Import-Module WebAdministration
+}
+catch {
+    Write-Error "Error al importar el módulo WebAdministration. Asegúrese de que IIS esté instalado y el módulo esté disponible."
+    return
+}
+
+Write-Host "Configurando el documento por defecto para el sitio: '$siteName'"
+
+# Configuramos el documento por defecto
+try {
+    Add-WebConfigurationProperty -Filter "system.webServer/defaultDocument/files" -PSPath "IIS:\Sites\$siteName" -AtIndex 0 -Name "Collection" -Value $defaultDocument
+    Write-Host "Documento por defecto configurado satisfactoriamente: $defaultDocument"
+} catch {
+    Write-Error "Error al configurar el documento por defecto: `$_"
+    return
+}
+"@
+
+##########################################################################
+# Cofificación del script
+##########################################################################
+
+# Codificar en Base64 para evitar problemas con caracteres especiales
+$encodedScript = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($remoteScript))
+
+##########################################################################
+# Ejecutar el script remoto
+##########################################################################
+
+# Ejecutar remotamente, suprimiendo streams de información para no saturar la salida
+ssh $sshUser@$ipServer "powershell -EncodedCommand $encodedScript" 2>$null 6>$null
